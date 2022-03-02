18aa8b9ef5ec5441086c779a5f24cce40405f8a3
Nathan Podevin <nathanpodevindev@gmail.com>
2022-03-02 16:23:18 +0100

    ajout des produits sur la page d'accueil avec le script

--- front/js/script.js
+++ front/js/script.js
@@ -2,16 +2,37 @@
 let products = []
 
 /* Je récupère les produits de mon Api avec Get*/
-fetch("http://localhost:3000/api/products/")
-  .then(function(res) {
-    if (res.ok) {
-      return res.json();
+async function getApi() {
+    await fetch("http://localhost:3000/api/products/")
+        .then(function(res) {
+            if (res.ok) {
+                return res.json();
+            }
+        })
+        .then(function(data) { 
+                return products = data;
+        })
+        .catch(function(err) {
+            console.log("une erreur est survenue", err)
+            // Une erreur est survenue
+        })
+};
+
+/* Fonction pour créer mes cartes produits sur la page d'accueil */
+async function productCard() {
+    await getApi();
+    let items = document.getElementById("items");
+    for (let i = 0; i < products.length; i++) {
+        items.innerHTML +=
+            `<a href="./product.html?id=${products[i]._id}">
+              <article>
+                <img src="${products[i].imageUrl}" alt="${products[i].altTxt}">
+                <h3 class="productName">${products[i].name}</h3>
+                <p class="productDescription">${products[i].description}</p>
+              </article>
+            </a>`;
     }
-  })
-  .then(function(data) { 
-    return products = data;
-  })
-  .catch(function(err) {
-    console.log("une erreur est survenue", err)
-    // Une erreur est survenue
-});
+    const productTemplate = document.querySelector("#items a");
+    productTemplate.style.display = "none";
+}
+productCard();
